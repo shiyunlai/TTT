4726730a6e5c763e4a00244426003b07b3936b38
浮沉丶 <37790169+zhoushihao95@users.noreply.github.com>
2018-05-09 17:06:15 +0800

    Sysdict Sysdictitem
    提交Sysdict Sysdictitem 相关内容

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/service/ISysDictService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/service/ISysDictService.java
@@ -2,14 +2,120 @@ package org.tis.tools.abf.module.sys.service;
 
 import com.baomidou.mybatisplus.service.IService;
 import org.tis.tools.abf.module.sys.entity.SysDict;
+import org.tis.tools.abf.module.sys.entity.SysDictItem;
+import org.tis.tools.abf.module.sys.exception.SysManagementException;
+
+import java.util.List;
 
 /**
- * sysDict的Service接口类
- * 
- * @author Auto Generate Tools
- * @date 2018/04/23
+ * <pre>
+ * 业务菜单服务
+ * </pre>
+ * @author megapro
+ *
  */
-public interface ISysDictService extends IService<SysDict>  {
+public interface ISysDictService extends IService<SysDict> {
+    /**
+     * <pre>
+     * 新增业务字典
+     * 系统自动补充guid
+     *
+     * </pre>
+     * @param  sysDict
+     * @return 新增的业务字典记录
+     * @throws SysManagementException
+     */
+    SysDict addDict(SysDict sysDict) throws SysManagementException;
+
+    /**
+     * <pre>
+     * 查询指定dictKey对应的业务字典信息（包括子业务字典信息）
+     * 一次查询可获取的信息包括：
+     * 1. 业务字典
+     * 2. 字典项
+     * 3. 子业务字典
+     *
+     * </pre>
+     * @param id 字典KEY
+     * @return 字典信息视图对象(SysDictDetail)
+     * @throws SysManagementException
+     */
+    SysDict queryDictDetail( String id ) throws SysManagementException;
+
+    /**
+     * <pre>
+     * 查询指定dictKey对应的业务字典信息（本身内容）
+     * 一次查询可获取的信息包括：
+     * 1. 业务字典
+     * 2. 字典项
+     *
+     * </pre>
+     * @param id 字典KEY
+     * @return 对应字典（SysDict）
+     * @throws SysManagementException
+     */
+    List<SysDict> queryDict( String id ) throws SysManagementException;
+
+    /**
+     * 修改业务字典
+     * @param sysDict
+     * @throws SysManagementException
+     */
+    SysDict editSysDict(SysDict sysDict) throws SysManagementException;
+
+
+    /**
+     * 根据业务字典GUID删除业务字典
+     *
+     * @param id
+     * 			业务字典GUID
+     * @throws SysManagementException
+     */
+    SysDict deleteDict(String id) throws SysManagementException;
+
+    /**
+     * 查询所有业务字典
+     *
+     * @param
+     * @return
+     * @throws SysManagementException
+     */
+    List<SysDict> querySysDicts() throws SysManagementException;
+
+    /**
+     * 根据GUID查询业务字典自身
+     * @param id
+     * 			业务字典GUID
+     * @return
+     * @throws SysManagementException
+     */
+    SysDict queryOneSysDictByGuid(String id) throws SysManagementException;
+
+    /**
+     * 设置业务字典的默认字典项
+     *
+     * @param dictGuid 字典GUID
+     * @param itemValue 默认值
+     * @return
+     * @throws SysManagementException
+     */
+    SysDict setDefaultDictValue(String dictGuid, String itemValue) throws SysManagementException;
+
+    /**
+     * 设置业务字典的默认字典项
+     *
+     * @param sysDict 字典GUID
+     * @return
+     * @throws SysManagementException
+     */
+    List<SysDict> dictKeyQuery(SysDict sysDict) throws SysManagementException;
+    /**
+     * 设置业务字典的默认字典项
+     *
+     * @param id 字典GUID
+     * @return
+     * @throws SysManagementException
+     */
+    SysDict querySysDictByGuid(String id) throws SysManagementException;
 
 }
-
--- tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/service/ISysRunConfigService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/service/ISysRunConfigService.java
@@ -2,14 +2,85 @@ package org.tis.tools.abf.module.sys.service;
 
 import com.baomidou.mybatisplus.service.IService;
 import org.tis.tools.abf.module.sys.entity.SysRunConfig;
+import org.tis.tools.abf.module.sys.exception.SysManagementException;
 
-/**
- * sysRunConfig的Service接口类
- * 
- * @author Auto Generate Tools
- * @date 2018/04/23
- */
-public interface ISysRunConfigService extends IService<SysRunConfig>  {
+import java.util.List;
+
+public interface ISysRunConfigService extends IService<SysRunConfig>{
+    /**
+     * 查询所有系统运行参数
+     *
+     * @return 系统运行参数集合
+     * @throws SysManagementException
+     */
+    List<SysRunConfig> queryAllSysRunConfig(String guid) throws SysManagementException;
+
+
+    /**
+     * <p>新增系统运行参数</p>
+     *
+     * <pre>
+     *     验证必输字段：
+     *          1.应用GUID:’guidApp’;
+     *          2.参数组别:’groupName’;
+     *          3.参数键: ‘keyName’ ;
+     *          4.值来源类型:’valueFrom’ ;
+     *          5.参数值:’value’ ;
+     *
+     *     服务端业务逻辑：
+     *          1.生成主键GUID，通过方法GUID.runConfig(）
+     * </pre>
+     *
+     * @param   guidApp
+     * @param   groupName
+     * @param   keyName
+     * @param   valueFrom
+     * @param   value
+     * @param   description
+     * @return  SysRunConfig
+     * @throws SysManagementException
+     */
+    SysRunConfig createSysRunConfig(String guidApp,String groupName,String keyName,String valueFrom,String value,String description) throws SysManagementException;
+
+    /**
+     * <p>修改系统运行参数</p>
+     *
+     * <pre>
+     *     验证必输字段：
+     *          1.应用GUID:’guidApp’;
+     *          2.参数组别:’groupName’;
+     *          3.参数键: ‘keyName’ ;
+     *          4.值来源类型:’valueFrom’ ;
+     *          5.参数值:’value’ ;
+     *
+     *     服务端业务逻辑：
+     *          无
+     * </pre>
+     * @param   guid
+     * @param   guidApp
+     * @param   groupName
+     * @param   keyName
+     * @param   valueFrom
+     * @param   value
+     * @param   description
+     * @return  SysRunConfig
+     * @throws SysManagementException
+     */
+    SysRunConfig editSysRunConfig(String guid,String guidApp,String groupName,String keyName,String valueFrom,String value,String description) throws SysManagementException;
+
+    /**
+     * <p>删除系统运行参数</p>
+     *
+     * <pre>
+     *     验证必输字段：
+     *          1.系统运行参数GUID:’guid’;
+     *
+     *     服务端业务逻辑：
+     *          无
+     *
+     * @param guid
+     * @throws SysManagementException
+     */
+    SysRunConfig deleteSysRunConfig(String guid) throws SysManagementException;
 
 }
-
