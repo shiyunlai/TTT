80665e0d3dd91771555f4df8bc2fef42926ccf72
xie-qing <15111413862@163.com>
2018-05-15 19:31:53 +0800

    菜单功能模块代码提交

---
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/impl/AcMenuServiceExt.java
@@ -0,0 +1,29 @@
+package org.tis.tools.abf.module.ac.service.impl;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tis.tools.abf.module.ac.dao.AcMenuMapper;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by ministrator on 2018/5/9.
+ */
+@Service
+public class AcMenuServiceExt implements Serializable{
+    @Autowired
+    AcMenuMapper AcMenuMapper;
+    /**
+     * 重新排序菜单下的子菜单
+     *
+     * @param targetGuid 目标菜单GUID
+     * @param index 起始位置
+     * @param flag 自增或自减
+     */
+    public void reorderMenu(String targetGuid, BigDecimal index, String flag) {
+        AcMenuMapper.reorderMenu(targetGuid, index, flag);
+    }
+
+}
