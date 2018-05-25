f1949f1966524e57ee2f2e584c13d0b98c0f7b21
chenchao92 <Cc_Object@163.com>
2018-05-25 17:31:49 +0800

    去掉了有关功能组的接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/IAcFuncService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/IAcFuncService.java
@@ -1,11 +1,8 @@
 package org.tis.tools.abf.module.ac.service;
 
-import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import org.tis.tools.abf.module.ac.entity.AcFunc;
 import org.tis.tools.abf.module.ac.exception.AcManagementException;
-
-import java.util.List;
 
 /**
  * acFunc的Service接口类
@@ -16,21 +13,7 @@ import java.util.List;
 public interface IAcFuncService extends IService<AcFunc>  {
 
     /**
-     * 新增功能父节点
-     * @param guidApp       隶属应用GUID
-     * @param funcType      功能类型
-     * @param funcCode      功能编号
-     * @param funcName      功能名称
-     * @param displayOrder  显示顺序
-     * @param funcDesc      功能描述
-     * @return              AcFunc
-     * @throws AcManagementException
-     */
-    AcFunc creatRootFunc(String guidApp,String funcType,String funcCode,String funcName,String displayOrder,
-                         String funcDesc,String isopen,String ischeck) throws AcManagementException;
-
-    /**
-     * 新增功能子节点
+     * 新增功能行为
      * @param guidApp       隶属应用GUID
      * @param funcType      功能类型
      * @param funcCode      功能编号
@@ -45,7 +28,7 @@ public interface IAcFuncService extends IService<AcFunc>  {
                      String funcDesc,String guidFunc,String isopen,String ischeck) throws AcManagementException;
 
     /**
-     * 修改功能
+     * 修改功能行为
      * @param guid          功能GUID
      * @param guidApp       隶属应用GUID
      * @param funcType      功能类型
@@ -62,27 +45,6 @@ public interface IAcFuncService extends IService<AcFunc>  {
     AcFunc changeFunc(String guid, String guidApp, String funcType, String funcCode, String funcName, String funcDesc,
                       String isopen, String ischeck, String displayOrder, String guidFunc)throws AcManagementException;
 
-
-    /**
-     * 删除根功能
-     * @param id
-     * @return Boolean
-     */
-    Boolean clearRootFunc(String id,String statusData)throws AcManagementException;
-
-    /**
-     * 查询某个根功能列表
-     * @param id
-     * @return List<AcFunc>
-     */
-    List<AcFunc> queryFuncList(String id)throws AcManagementException;
-
-    /**
-     * 查询列表
-     * @param page
-     * @return
-     */
-    List<Object> getlist(Page<AcFunc> page);
 
 }
 
