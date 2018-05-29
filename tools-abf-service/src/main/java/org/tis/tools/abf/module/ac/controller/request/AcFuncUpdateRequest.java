7751cab6af26b1051b645447fa1b2458aadd330e
chenchao92 <Cc_Object@163.com>
2018-05-29 17:32:06 +0800

    修改了枚举值对应的枚举类

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/controller/request/AcFuncUpdateRequest.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/controller/request/AcFuncUpdateRequest.java
@@ -1,9 +1,15 @@
 package org.tis.tools.abf.module.ac.controller.request;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.NotBlank;
+import org.tis.tools.abf.module.ac.entity.enums.FuncType;
+import org.tis.tools.abf.module.common.entity.enums.YON;
+import org.tis.tools.core.entity.enums.CommonEnumDeserializer;
 import org.tis.tools.core.entity.request.RestRequest;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by chenchao
@@ -18,8 +24,9 @@ public class AcFuncUpdateRequest extends RestRequest {
     @NotBlank(message = "隶属应用ID不能为空")
     private String guidApp;
 
-    @NotBlank(message = "功能类型不能为空")
-    private String funcType;
+    @NotNull(message = "功能类型不能为空")
+    @JSONField(deserializeUsing = CommonEnumDeserializer.class)
+    private FuncType funcType;
 
     @NotBlank(message = "功能编号不能为空")
     private String funcCode;
@@ -32,11 +39,13 @@ public class AcFuncUpdateRequest extends RestRequest {
 
     private String guidFunc;
 
-    @NotBlank(message = "是否启用不能为空")
-    private String isopen;
+    @NotNull(message = "是否启用不能为空")
+    @JSONField(deserializeUsing = CommonEnumDeserializer.class)
+    private YON isopen;
 
-    @NotBlank(message = "是否验证权限不能为空")
-    private String ischeck;
+    @NotNull(message = "是否验证权限不能为空")
+    @JSONField(deserializeUsing = CommonEnumDeserializer.class)
+    private YON ischeck;
 
     private String funcDesc;
 
