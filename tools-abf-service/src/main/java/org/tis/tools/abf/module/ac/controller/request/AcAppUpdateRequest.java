7d7fda9a3780876bd3ce8db9c7bd49a818bbff0e
chenchao92 <Cc_Object@163.com>
2018-05-29 17:26:25 +0800

    配置了相应的枚举对象

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/controller/request/AcAppUpdateRequest.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/controller/request/AcAppUpdateRequest.java
@@ -1,10 +1,15 @@
 package org.tis.tools.abf.module.ac.controller.request;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.NotBlank;
+import org.tis.tools.abf.module.ac.entity.enums.AcAppType;
+import org.tis.tools.abf.module.common.entity.enums.YON;
+import org.tis.tools.core.entity.enums.CommonEnumDeserializer;
 import org.tis.tools.core.entity.request.RestRequest;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 /**
@@ -22,11 +27,13 @@ public class AcAppUpdateRequest extends RestRequest{
     @NotBlank(message = "应用名称不能为空")
     private String appName;
 
-    @NotBlank(message = "应用类型不能为空")
-    private String appType;
+    @NotNull(message = "应用类型不能为空")
+    @JSONField(deserializeUsing = CommonEnumDeserializer.class)
+    private AcAppType appType;
 
-    @NotBlank(message = "是否开通不能为空")
-    private String isopen;
+    @NotNull(message = "是否开通不能为空")
+    @JSONField(deserializeUsing = CommonEnumDeserializer.class)
+    private YON isopen;
 
     private String guid;
 
