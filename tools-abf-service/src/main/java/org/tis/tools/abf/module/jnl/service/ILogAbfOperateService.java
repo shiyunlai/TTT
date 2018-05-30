196110e6057b88ffbd3e41554ec3bb3297cac081
chenchao92 <Cc_Object@163.com>
2018-05-30 10:00:05 +0800

    新增了根据条件查询日志的方法

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/service/ILogAbfOperateService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/service/ILogAbfOperateService.java
@@ -1,13 +1,15 @@
 package org.tis.tools.abf.module.jnl.service;
 
-import org.tis.tools.abf.module.jnl.entity.LogAbfOperate;
 import com.baomidou.mybatisplus.service.IService;
+import org.tis.tools.abf.module.jnl.entity.LogAbfOperate;
 import org.tis.tools.abf.module.jnl.entity.vo.LogOperateDetail;
 import org.tis.tools.abf.module.jnl.exception.OperateLogException;
 
+import java.util.List;
+
 /**
  * logAbfOperate的Service接口类
- * 
+ *
  * @author Auto Generate Tools
  * @date 2018/05/14
  */
@@ -19,6 +21,14 @@ public interface ILogAbfOperateService extends IService<LogAbfOperate>  {
      * @throws OperateLogException
      */
     void insertOperatorLog(LogOperateDetail log) throws OperateLogException;
+
+    /**
+     * 根据条件查询日志
+     * @param condition
+     * @return List<LogAbfOperate>
+     * @throws OperateLogException
+     */
+    List<LogAbfOperate> queryByCondition(String condition) throws OperateLogException;
 
 }
 
