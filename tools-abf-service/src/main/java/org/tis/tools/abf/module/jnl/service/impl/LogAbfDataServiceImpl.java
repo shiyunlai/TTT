5509f8d4a749ce3abc9e750b92d9770a9b8285b3
chenchao92 <Cc_Object@163.com>
2018-05-30 10:00:26 +0800

    实现了新增接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/service/impl/LogAbfDataServiceImpl.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/service/impl/LogAbfDataServiceImpl.java
@@ -1,11 +1,19 @@
 package org.tis.tools.abf.module.jnl.service.impl;
 
-import org.tis.tools.abf.module.jnl.service.ILogAbfDataService;
-import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.tis.tools.abf.module.jnl.dao.LogAbfDataMapper;
 import org.tis.tools.abf.module.jnl.entity.LogAbfData;
-import org.springframework.transaction.annotation.Transactional;
+import org.tis.tools.abf.module.jnl.exception.OperateLogException;
+import org.tis.tools.abf.module.jnl.service.ILogAbfDataService;
+import org.tis.tools.core.exception.i18.ExceptionCodes;
+
+import java.util.List;
+
+import static org.tis.tools.core.utils.BasicUtil.wrap;
 
 /**
  * logAbfData的Service接口实现类
@@ -17,5 +25,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = Exception.class)
 public class LogAbfDataServiceImpl extends ServiceImpl<LogAbfDataMapper, LogAbfData> implements ILogAbfDataService {
 
+    /**
+     * 查询
+     * @param operateGuid
+     * @return
+     */
+    @Override
+    public List<LogAbfData> queryForOperate(String operateGuid) throws OperateLogException {
+
+        if (null == operateGuid){
+            throw  new OperateLogException(ExceptionCodes.NOT_ALLOW_NULL_WHEN_QUERY,wrap("","LOG_DATA_QUERY_FOR_OPERATE"));
+        }
+
+        Wrapper<LogAbfData> wrapper = new EntityWrapper<LogAbfData>();
+        wrapper.eq(LogAbfData.COLUMN_GUID_OPERATE,operateGuid);
+
+        List<LogAbfData> logAbfDataList = selectList(wrapper);
+
+        return logAbfDataList;
+    }
 }
 
