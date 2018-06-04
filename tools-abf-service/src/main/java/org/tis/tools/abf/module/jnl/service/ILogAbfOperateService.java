3669f1c9302a0693de7c2dd67b1febca879a7ef6
chenchao92 <Cc_Object@163.com>
2018-06-04 10:58:27 +0800

    新增了对某人某天的查询接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/service/ILogAbfOperateService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/service/ILogAbfOperateService.java
@@ -1,11 +1,11 @@
 package org.tis.tools.abf.module.jnl.service;
 
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import org.tis.tools.abf.module.jnl.entity.LogAbfOperate;
 import org.tis.tools.abf.module.jnl.entity.vo.LogOperateDetail;
 import org.tis.tools.abf.module.jnl.exception.OperateLogException;
-
-import java.util.List;
 
 /**
  * logAbfOperate的Service接口类
@@ -25,10 +25,19 @@ public interface ILogAbfOperateService extends IService<LogAbfOperate>  {
     /**
      * 查询某天的日志
      * @param condition
-     * @return List<LogAbfOperate>
+     * @return Page<LogAbfOperate>
      * @throws OperateLogException
      */
-    List<LogAbfOperate> queryByCondition(String condition) throws OperateLogException;
+    Page<LogAbfOperate> queryByCondition(Page<LogAbfOperate> page, Wrapper<LogAbfOperate> wrapper, String condition) throws
+            OperateLogException;
+
+    /**
+     * 查询某人某天的日志
+     * @param operateTime
+     * @param userId
+     * @return
+     */
+    Page<LogAbfOperate> queryByTimeAndUser(Page<LogAbfOperate> page, Wrapper<LogAbfOperate> wrapper,String operateTime, String userId) throws OperateLogException;
 
 }
 
