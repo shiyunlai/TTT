e890269ae72ed4a9cdfad4df9e09c366e8ef578f
chenchao92 <Cc_Object@163.com>
2018-06-04 17:29:08 +0800

    删除了多于的查询接口,新增了根据userId查询列表的接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/controller/LogAbfOperateController.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/controller/LogAbfOperateController.java
@@ -1,13 +1,10 @@
 package org.tis.tools.abf.module.jnl.controller;
 
-import io.swagger.annotations.ApiOperation;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.tis.tools.abf.module.jnl.annotation.OperateLog;
-import org.tis.tools.abf.module.jnl.controller.request.LogAbfOperateRequest;
 import org.tis.tools.abf.module.jnl.entity.LogAbfOperate;
-import org.tis.tools.abf.module.jnl.entity.enums.OperateType;
 import org.tis.tools.abf.module.jnl.service.ILogAbfOperateService;
 import org.tis.tools.core.web.controller.BaseController;
 import org.tis.tools.core.web.vo.ResultVO;
@@ -25,22 +22,25 @@ public class LogAbfOperateController extends BaseController<LogAbfOperate> {
     @Autowired
     private ILogAbfOperateService logAbfOperateService;
 
-    @OperateLog(type = OperateType.QUERY ,desc = "查询某天的日志")
-    @ApiOperation(value = "查询某天的日志" , notes = "传入操作时间字段,传入格式yyyyMMdd")
-    @PostMapping
-    public ResultVO queryOneDay(@RequestBody @Validated SmartPage<LogAbfOperate> page, @RequestBody @Validated
-            LogAbfOperateRequest logAbfOperateRequest){
-        return ResultVO.success("查询成功",logAbfOperateService.queryByCondition(getPage(page), getCondition(page),logAbfOperateRequest.getOperateTime()));
-    }
+   @PostMapping("/list")
+   public ResultVO list(@RequestBody @Validated SmartPage<LogAbfOperate> page){
+            return ResultVO.success("查询成功",logAbfOperateService.selectPage(getPage(page),getCondition(page)));
+   }
 
-    @OperateLog(type = OperateType.QUERY ,desc = "查询某天某个操作员的日志")
-    @ApiOperation(value = "查询某天某个操作员的日志" , notes = "传入操作员id字段;传入操作时间字段,传入格式yyyyMMdd")
-    @PostMapping("/list")
-    public ResultVO queryUserByDay(@RequestBody @Validated SmartPage<LogAbfOperate> page,@RequestBody @Validated LogAbfOperateRequest logAbfOperateRequest){
-        if (null == logAbfOperateRequest.getUserId() || "".equals(logAbfOperateRequest.getUserId())){
-            return ResultVO.error("404","操作员不能为空!");
-        }
+   @GetMapping("/{id}")
+   public ResultVO detail(@PathVariable @NotBlank(message = "GUID不能为空") String id){
+       LogAbfOperate logAbfOperate = logAbfOperateService.selectById(id);
+       if (logAbfOperate == null) {
+           return ResultVO.error("404", "找不到对应记录或已经被删除！");
+       }
 
-        return ResultVO.success("查询成功",logAbfOperateService.queryByTimeAndUser(getPage(page), getCondition(page),logAbfOperateRequest.getOperateTime(),logAbfOperateRequest.getUserId()));
-    }
+       return ResultVO.success("查询成功",logAbfOperate);
+   }
+
+   @PostMapping("/{id}")
+   public ResultVO listByUser(@RequestBody @Validated SmartPage<LogAbfOperate> page,@PathVariable @NotBlank(message =
+           "操作员ID不能为空") String id){
+       return ResultVO.success("查询成功",logAbfOperateService.queryListByUser(getPage(page),getCondition(page),id));
+   }
+
 }
