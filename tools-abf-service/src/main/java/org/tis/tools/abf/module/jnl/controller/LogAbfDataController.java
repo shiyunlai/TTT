56ff5c12e18a1443cd213594848a5c1e98ad0613
chenchao92 <Cc_Object@163.com>
2018-06-04 17:28:20 +0800

    新增了根据id的查询接口和列表查询接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/controller/LogAbfDataController.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/controller/LogAbfDataController.java
@@ -4,9 +4,7 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.tis.tools.abf.module.jnl.annotation.OperateLog;
 import org.tis.tools.abf.module.jnl.entity.LogAbfData;
-import org.tis.tools.abf.module.jnl.entity.enums.OperateType;
 import org.tis.tools.abf.module.jnl.service.ILogAbfDataService;
 import org.tis.tools.core.web.controller.BaseController;
 import org.tis.tools.core.web.vo.ResultVO;
@@ -24,9 +22,23 @@ public class LogAbfDataController extends BaseController<LogAbfData> {
     @Autowired
     private ILogAbfDataService logAbfDataService;
 
-    @OperateLog(type = OperateType.QUERY,desc = "根据操作日志GUID查询")
     @PostMapping("/{id}")
     public ResultVO list(@RequestBody @Validated SmartPage<LogAbfData> page , @PathVariable @NotBlank(message = "操作日志GUID不能为空") String id) {
         return  ResultVO.success("查询成功", logAbfDataService.queryPageById(getPage(page), getCondition(page),id));
     }
+
+    @GetMapping("/{id}")
+    public ResultVO detial(@PathVariable @NotBlank(message = "操作日志GUID不能为空") String id){
+        LogAbfData logAbfData = logAbfDataService.selectById(id);
+        if (logAbfData == null) {
+            return ResultVO.error("404", "找不到对应记录或已经被删除！");
+        }
+        return ResultVO.success("查询成功",logAbfData);
+    }
+
+    @GetMapping("/list/{dataGuid}")
+    public ResultVO listByDataId(@PathVariable @NotBlank(message = "操作日志GUID不能为空") String dataGuid){
+        return ResultVO.success("查询成功",logAbfDataService.queryByDataId(dataGuid));
+    }
+
 }
