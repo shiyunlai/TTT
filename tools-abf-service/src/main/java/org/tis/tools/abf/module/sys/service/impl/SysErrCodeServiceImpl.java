b4597c2c36762da6f256be1e1460dfbb1232f389
chenchao92 <Cc_Object@163.com>
2018-06-04 11:10:01 +0800

    新增了add接口的实现方法

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/service/impl/SysErrCodeServiceImpl.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/service/impl/SysErrCodeServiceImpl.java
@@ -1,11 +1,16 @@
 package org.tis.tools.abf.module.sys.service.impl;
 
+import org.tis.tools.abf.module.sys.controller.request.SysErrCodeRequest;
 import org.tis.tools.abf.module.sys.entity.SysErrCode;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import org.tis.tools.abf.module.sys.dao.SysErrCodeMapper;
+import org.tis.tools.abf.module.sys.exception.SYSExceptionCodes;
+import org.tis.tools.abf.module.sys.exception.SysManagementException;
 import org.tis.tools.abf.module.sys.service.ISysErrCodeService;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.tis.tools.core.utils.BasicUtil.wrap;
 
 /**
  * sysErrCode的Service接口实现类
@@ -17,5 +22,46 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = Exception.class)
 public class SysErrCodeServiceImpl extends ServiceImpl<SysErrCodeMapper, SysErrCode> implements ISysErrCodeService {
 
+    @Override
+    public void add(SysErrCodeRequest sysErrCodeRequest) throws SysManagementException {
+
+        SysErrCode sysErrCode = new SysErrCode();
+
+        //收集参数
+        sysErrCode.setErrCode(sysErrCodeRequest.getErrCode());
+        sysErrCode.setErrcodeKind(sysErrCodeRequest.getErrcodeKind());
+        sysErrCode.setErrMsg(sysErrCodeRequest.getErrMsg());
+
+        try {
+                insert(sysErrCode);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new SysManagementException(SYSExceptionCodes.FAILURE_WHEN_INSERT,
+                    wrap(e.getMessage()));
+        }
+
+    }
+
+    @Override
+    public SysErrCode change(SysErrCodeRequest sysErrCodeRequest) throws SysManagementException {
+
+        SysErrCode sysErrCode = new SysErrCode();
+
+        //收集参数
+        sysErrCode.setGuid(sysErrCodeRequest.getGuid());
+        sysErrCode.setErrCode(sysErrCodeRequest.getErrCode());
+        sysErrCode.setErrcodeKind(sysErrCodeRequest.getErrcodeKind());
+        sysErrCode.setErrMsg(sysErrCodeRequest.getErrMsg());
+
+        try {
+            updateById(sysErrCode);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new SysManagementException(SYSExceptionCodes.FAILURE_WHEN_UPDATE,
+                    wrap(e.getMessage()));
+        }
+
+        return sysErrCode;
+    }
 }
 
