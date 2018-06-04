68aa23a05699c9443f3d4f9ce4e6d63243c031dc
chenchao92 <Cc_Object@163.com>
2018-06-04 10:56:40 +0800

    为新增和修改方法添加判断确保user_id的唯一性

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/impl/AcOperatorServiceImpl.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/impl/AcOperatorServiceImpl.java
@@ -1,16 +1,18 @@
 package org.tis.tools.abf.module.ac.service.impl;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.tis.tools.abf.module.ac.controller.request.AcOperatorAddRequest;
 import org.tis.tools.abf.module.ac.dao.AcOperatorMapper;
 import org.tis.tools.abf.module.ac.entity.AcOperator;
-import org.tis.tools.abf.module.ac.entity.enums.AuthMode;
-import org.tis.tools.abf.module.ac.entity.enums.OperatorStatus;
 import org.tis.tools.abf.module.ac.exception.AcOperatorManagementException;
 import org.tis.tools.abf.module.ac.service.IAcOperatorService;
 import org.tis.tools.core.exception.i18.ExceptionCodes;
+
+import java.util.List;
 
 import static org.tis.tools.core.utils.BasicUtil.wrap;
 
@@ -37,13 +39,30 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
     @Override
     public boolean addAcOperator(AcOperatorAddRequest request) throws AcOperatorManagementException {
 
+        boolean isexist = new Boolean(false);
+
+        try {
+
+        //判断user_id是否已经存在,确保user_id的唯一性
+        Wrapper<AcOperator> wrapper = new EntityWrapper<AcOperator>();
+        List<AcOperator > lists = selectList(wrapper);
+
+        for (AcOperator acOperator:lists) {
+            if (request.getUserId().equals(acOperator.getUserId())){
+                return isexist;
+            }
+        }
+        //循环结束之后表示没有重复,故设置标志位为true
+        isexist = true;
+
+        //收集参数
         AcOperator acOperator = new AcOperator();
         acOperator.setUserId(request.getUserId());
         acOperator.setPassword(request.getPassword());
         acOperator.setOperatorName(request.getOperatorName());
-        acOperator.setOperatorStatus(OperatorStatus.valueOf(request.getOperatorStatus()));
+        acOperator.setOperatorStatus(request.getOperatorStatus());
         acOperator.setInvalDate(request.getInvalDate());
-        acOperator.setAuthMode(AuthMode.valueOf(request.getAuthMode()));
+        acOperator.setAuthMode(request.getAuthMode());
         acOperator.setLockLimit(request.getLockLimit());
         acOperator.setErrCount(request.getErrCount());
         acOperator.setLockTime(request.getLockTime());
@@ -54,9 +73,10 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
         acOperator.setValidTime(request.getValidTime());
         acOperator.setMacCode(request.getMacCode());
         acOperator.setIpAddress(request.getIpAddress());
-        try {
-            boolean b = insert(acOperator);
-            return b;
+
+        insert(acOperator);
+
+        return isexist;
         }catch (Exception e){
             throw new AcOperatorManagementException(ExceptionCodes.FAILURE_WHEN_INSERT,wrap("ACOPERATOR",e));
         }
@@ -70,9 +90,25 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
      */
     @Override
     public boolean updateAcOperatorByCondition(AcOperator acOperator) throws AcOperatorManagementException {
+
+        boolean isexist = new Boolean(false);
+
         try {
-            boolean b = updateById(acOperator);
-            return b;
+
+                //判断user_id是否已经存在,确保user_id的唯一性
+                Wrapper<AcOperator> wrapper = new EntityWrapper<AcOperator>();
+                List<AcOperator > lists = selectList(wrapper);
+
+                for (AcOperator acOperatorNew:lists) {
+                    if (acOperator.getUserId().equals(acOperatorNew.getUserId()) && !(acOperator.getGuid().equals(acOperatorNew.getGuid())) ){
+                        return isexist;
+                    }
+                }
+                //循环结束之后表示没有重复,故设置标志位为true
+                isexist = true;
+
+            updateById(acOperator);
+            return isexist;
         }catch (Exception e){
             throw new AcOperatorManagementException(ExceptionCodes.FAILURE_WHEN_UPDATE,wrap("ACOPERATOR",e));
         }
