e38fdb94aef6382bcd2ca81f380f9c5b67105a7f
chenchao92 <Cc_Object@163.com>
2018-06-04 11:08:00 +0800

    添加了公共字段

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/entity/SysErrCode.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/sys/entity/SysErrCode.java
@@ -1,9 +1,12 @@
 package org.tis.tools.abf.module.sys.entity;
 
-import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.annotations.*;
+import com.baomidou.mybatisplus.enums.FieldFill;
 import lombok.Data;
-import com.baomidou.mybatisplus.annotations.TableId;
+import org.tis.tools.abf.module.sys.entity.enums.ErrCodeKind;
+
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * sysErrCode记录系统中的各种错误代码信息，如系统抛出的错误信息，交易执行时的错误码等
@@ -46,6 +49,26 @@ public class SysErrCode implements Serializable {
     public static final String COLUMN_ERR_MSG = "err_msg";
 
     /**
+     * createtime对应表字段
+     */
+    public static final String COLUMN_CREATETIME = "createtime";
+
+    /**
+     * lastupdate对应表字段
+     */
+    public static final String COLUMN_LASTUPDATE = "lastupdate";
+
+    /**
+     * updator对应表字段
+     */
+    public static final String COLUMN_UPDATOR = "updator";
+
+    /**
+     * dataStatus对应表字段
+     */
+    public static final String COLUMN_DATA_STATUS = "data_status";
+
+    /**
      * guid逻辑名
      */
     public static final String NAME_GUID = "数据主键";
@@ -66,6 +89,26 @@ public class SysErrCode implements Serializable {
     public static final String NAME_ERR_MSG = "错误信息";
 
     /**
+     * createtime逻辑名
+     */
+    public static final String NAME_CREATETIME = "创建时间";
+
+    /**
+     * lastupdate逻辑名
+     */
+    public static final String NAME_LASTUPDATE = "最近更新时间";
+
+    /**
+     * updator逻辑名
+     */
+    public static final String NAME_UPDATOR = "最近更新人员";
+
+    /**
+     * dataStatus逻辑名
+     */
+    public static final String NAME_DATA_STATUS = "数据状态";
+
+    /**
      * 数据主键:全局唯一标识符（GUID，Globally Unique Identifier）
      */
     @TableId
@@ -76,7 +119,7 @@ public class SysErrCode implements Serializable {
      * SYS 系统错误码
      * TRANS 交易错误码
      */
-    private String errcodeKind;
+    private ErrCodeKind errcodeKind;
 
     /**
      * 错误代码
@@ -87,6 +130,33 @@ public class SysErrCode implements Serializable {
      * 错误信息
      */
     private String errMsg;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createtime;
+
+    /**
+     * 最近更新时间
+     */
+    @Version
+    @TableField(fill = FieldFill.UPDATE)
+    private Date lastupdate;
+
+    /**
+     * 最近更新人员
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updator;
+
+    /**
+     * 数据状态:0 有效
+     * D 删除（逻辑删除）
+     */
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    private String dataStatus;
 
 }
 
