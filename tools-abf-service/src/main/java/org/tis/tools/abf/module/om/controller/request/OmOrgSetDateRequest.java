b51c6dcfa1bb3093636d83a7d6e4b28354448604
chenchao92 <Cc_Object@163.com>
2018-06-05 22:15:16 +0800

    新增OmOrgSetDateRequest类

---
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/om/controller/request/OmOrgSetDateRequest.java
@@ -0,0 +1,27 @@
+package org.tis.tools.abf.module.om.controller.request;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.tis.tools.core.entity.request.RestRequest;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * Created by chenchao
+ * Created on 2018/6/5 15:01
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class OmOrgSetDateRequest extends RestRequest{
+
+    private String guid;
+
+    @NotNull(message = "生效日期不能为空")
+    private Date startDate;
+
+    @NotNull(message = "失效日期不能为空")
+    private Date endDate;
+
+
+}
