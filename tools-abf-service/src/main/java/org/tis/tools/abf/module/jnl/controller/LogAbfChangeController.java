1360e2c2c730612d7026dd739ad21aa9157cf72b
chenchao92 <Cc_Object@163.com>
2018-06-07 20:03:45 +0800

    删除了查询接口的OperateLog注解

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/controller/LogAbfChangeController.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/jnl/controller/LogAbfChangeController.java
@@ -4,9 +4,7 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.tis.tools.abf.module.jnl.annotation.OperateLog;
 import org.tis.tools.abf.module.jnl.entity.LogAbfChange;
-import org.tis.tools.abf.module.jnl.entity.enums.OperateType;
 import org.tis.tools.abf.module.jnl.service.ILogAbfChangeService;
 import org.tis.tools.core.web.controller.BaseController;
 import org.tis.tools.core.web.vo.ResultVO;
@@ -24,7 +22,6 @@ public class LogAbfChangeController extends BaseController<LogAbfChange> {
     @Autowired
     private ILogAbfChangeService logAbfChangeService;
 
-    @OperateLog(type = OperateType.QUERY,desc = "根据操作日志GUID查询")
     @PostMapping("/{id}")
     public ResultVO page(@RequestBody @Validated SmartPage<LogAbfChange> page , @PathVariable @NotBlank(message = "操作数据GUID不能为空") String id) {
         return  ResultVO.success("查询成功", logAbfChangeService.queryPageById(getPage(page), getCondition(page),id));
