53fd61aff6a27ccc4358c95fb90331699b57cbed
chenchao92 <Cc_Object@163.com>
2018-06-11 11:30:41 +0800

    添加了新增修改和通过身份id分页查询身份权限集的接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/IAcOperatorIdentityresService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/IAcOperatorIdentityresService.java
@@ -1,7 +1,11 @@
 package org.tis.tools.abf.module.ac.service;
 
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
+import org.tis.tools.abf.module.ac.controller.request.AcOperatorIdentityresRequest;
 import org.tis.tools.abf.module.ac.entity.AcOperatorIdentityres;
+import org.tis.tools.abf.module.ac.exception.AcManagementException;
 
 /**
  * acOperatorIdentityres的Service接口类
@@ -10,6 +14,32 @@ import org.tis.tools.abf.module.ac.entity.AcOperatorIdentityres;
  * @date 2018/04/23
  */
 public interface IAcOperatorIdentityresService extends IService<AcOperatorIdentityres>  {
+
+    /**
+     * 新增身份权限集
+     * @param acOperatorIdentityres
+     * @throws AcManagementException
+     */
+    void add(AcOperatorIdentityresRequest acOperatorIdentityres) throws AcManagementException;
+
+    /**
+     * 修改身份权限集
+     * @param acOperatorIdentityres
+     * @return
+     * @throws AcManagementException
+     */
+    AcOperatorIdentityres change(AcOperatorIdentityresRequest acOperatorIdentityres)throws AcManagementException;
+
+    /**
+     * 通过身份id分页查询身份权限集
+     * @param page
+     * @param wrapper
+     * @param id
+     * @return
+     * @throws AcManagementException
+     */
+    Page<AcOperatorIdentityres> queryByIdentity(Page<AcOperatorIdentityres> page, Wrapper<AcOperatorIdentityres>
+            wrapper,String id)throws AcManagementException;
 
 }
 
