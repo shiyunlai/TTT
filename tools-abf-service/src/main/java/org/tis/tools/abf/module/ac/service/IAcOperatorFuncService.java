87c424dd5536f932812c49ca5a3f1986de78d17d
chenchao92 <Cc_Object@163.com>
2018-06-11 11:30:04 +0800

    添加了新增修改接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/IAcOperatorFuncService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/IAcOperatorFuncService.java
@@ -1,7 +1,9 @@
 package org.tis.tools.abf.module.ac.service;
 
 import com.baomidou.mybatisplus.service.IService;
+import org.tis.tools.abf.module.ac.controller.request.AcOperatorFuncRequest;
 import org.tis.tools.abf.module.ac.entity.AcOperatorFunc;
+import org.tis.tools.abf.module.ac.exception.AcManagementException;
 
 /**
  * acOperatorFunc的Service接口类
@@ -10,6 +12,21 @@ import org.tis.tools.abf.module.ac.entity.AcOperatorFunc;
  * @date 2018/04/23
  */
 public interface IAcOperatorFuncService extends IService<AcOperatorFunc>  {
+
+    /**
+     * 新增操作员特殊权限配置
+     * @param acOperatorFuncRequestc
+     * @throws AcManagementException
+     */
+    void add(AcOperatorFuncRequest acOperatorFuncRequestc) throws AcManagementException;
+
+    /**
+     *修改操作员特殊权限配置
+     * @param acOperatorFuncRequestc
+     * @return
+     * @throws AcManagementException
+     */
+    AcOperatorFunc change(AcOperatorFuncRequest acOperatorFuncRequestc)throws AcManagementException;
 
 }
 
