2ad119b5fc540bd29e43700a2291b9e057b7ecd7
chenchao92 <Cc_Object@163.com>
2018-06-12 20:55:36 +0800

    添加了新增接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/om/service/IOmEmpOrgService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/om/service/IOmEmpOrgService.java
@@ -1,7 +1,9 @@
 package org.tis.tools.abf.module.om.service;
 
 import com.baomidou.mybatisplus.service.IService;
+import org.tis.tools.abf.module.om.controller.request.OmEmpOrgRequest;
 import org.tis.tools.abf.module.om.entity.OmEmpOrg;
+import org.tis.tools.abf.module.om.exception.OrgManagementException;
 
 /**
  * omEmpOrg的Service接口类
@@ -10,6 +12,22 @@ import org.tis.tools.abf.module.om.entity.OmEmpOrg;
  * @date 2018/04/23
  */
 public interface IOmEmpOrgService extends IService<OmEmpOrg>  {
+
+    /***
+     * 新增员工隶属机构关系表
+     * @param omEmpOrgRequest
+     * @throws OrgManagementException
+     */
+    void add(OmEmpOrgRequest omEmpOrgRequest) throws OrgManagementException;
+
+
+    /**
+     * 修改员工隶属机构关系表
+     * @param omEmpOrgRequest
+     * @return
+     * @throws OrgManagementException
+     */
+    OmEmpOrg change(OmEmpOrgRequest omEmpOrgRequest) throws OrgManagementException;
 
 }
 
