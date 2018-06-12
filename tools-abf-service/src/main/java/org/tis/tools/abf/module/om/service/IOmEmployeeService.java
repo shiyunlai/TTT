ae0ab285410992d9769a02b3154e9730d34e54be
chenchao92 <Cc_Object@163.com>
2018-06-12 20:54:46 +0800

    新增根据机构ID查询员工列表和分页查询员工接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/om/service/IOmEmployeeService.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/om/service/IOmEmployeeService.java
@@ -1,10 +1,14 @@
 package org.tis.tools.abf.module.om.service;
 
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import org.tis.tools.abf.module.om.controller.request.OmEmployeeAddRequest;
 import org.tis.tools.abf.module.om.controller.request.OmEmployeeUpdateRequest;
 import org.tis.tools.abf.module.om.entity.OmEmployee;
 import org.tis.tools.abf.module.om.exception.OrgManagementException;
+
+import java.util.List;
 
 /**
  * omEmployee的Service接口类
@@ -28,6 +32,25 @@ public interface IOmEmployeeService extends IService<OmEmployee>  {
      * @throws OrgManagementException
      */
     boolean change(OmEmployeeUpdateRequest omEmployeeUpdateRequest) throws OrgManagementException;
+
+    /**
+     * 根据机构ID分页查员工
+     * @param page
+     * @param wrapper
+     * @param id
+     * @return
+     * @throws OrgManagementException
+     */
+    Page<OmEmployee> queryEmpByOrg(Page<OmEmployee> page,Wrapper<OmEmployee> wrapper, String id)throws
+            OrgManagementException;
+
+    /**
+     * 根据机构id查询员工列表
+     * @param id
+     * @return
+     * @throws OrgManagementException
+     */
+    List<OmEmployee> queryEmpListByOrg(String id) throws OrgManagementException;
 
 }
 
