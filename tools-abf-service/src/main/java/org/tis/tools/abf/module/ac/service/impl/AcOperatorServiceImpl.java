16fa6423ac466792559855acd8ec50cc6ba56e89
chenchao92 <Cc_Object@163.com>
2018-06-14 16:48:32 +0800

    修改新增操作员的接口,新增查询所有操作员的实现方法

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/impl/AcOperatorServiceImpl.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/service/impl/AcOperatorServiceImpl.java
@@ -9,12 +9,14 @@ import org.tis.tools.abf.module.ac.controller.request.AcOperatorAddRequest;
 import org.tis.tools.abf.module.ac.controller.request.AcOperatorStatusRequest;
 import org.tis.tools.abf.module.ac.dao.AcOperatorMapper;
 import org.tis.tools.abf.module.ac.entity.AcOperator;
+import org.tis.tools.abf.module.ac.entity.enums.AuthMode;
 import org.tis.tools.abf.module.ac.entity.enums.OperatorStatus;
 import org.tis.tools.abf.module.ac.exception.AcExceptionCodes;
 import org.tis.tools.abf.module.ac.exception.AcOperatorManagementException;
 import org.tis.tools.abf.module.ac.service.IAcOperatorService;
 import org.tis.tools.core.exception.i18.ExceptionCodes;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.tis.tools.core.utils.BasicUtil.wrap;
@@ -58,15 +60,29 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
         //循环结束之后表示没有重复,故设置标志位为true
         isexist = true;
 
-        //收集参数
+
         AcOperator acOperator = new AcOperator();
+        //操作员状态默认是停用
+        acOperator.setOperatorStatus(OperatorStatus.stop);
+        //锁定次数前端不传值默认为5,否则为前段值
+            if ("".equals(request.getLockLimit())){
+                acOperator.setLockLimit(BigDecimal.valueOf(5));
+            }else {
+                acOperator.setLockLimit(request.getLockLimit());
+            }
+
+            if (null == request.getAuthMode() || AuthMode.password.equals(request.getAuthMode())){
+                acOperator.setAuthMode(AuthMode.password);
+                acOperator.setPassword("111111");
+            }else {
+                acOperator.setAuthMode(request.getAuthMode());
+                acOperator.setPassword(request.getPassword());
+            }
+
+        //收集参数
         acOperator.setUserId(request.getUserId());
-        acOperator.setPassword(request.getPassword());
         acOperator.setOperatorName(request.getOperatorName());
-        acOperator.setOperatorStatus(request.getOperatorStatus());
         acOperator.setInvalDate(request.getInvalDate());
-        acOperator.setAuthMode(request.getAuthMode());
-        acOperator.setLockLimit(request.getLockLimit());
         acOperator.setErrCount(request.getErrCount());
         acOperator.setLockTime(request.getLockTime());
         acOperator.setUnlockTime(request.getUnlockTime());
@@ -81,7 +97,8 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
 
         return isexist;
         }catch (Exception e){
-            throw new AcOperatorManagementException(ExceptionCodes.FAILURE_WHEN_INSERT,wrap("ACOPERATOR",e));
+            e.printStackTrace();
+            throw new AcOperatorManagementException(ExceptionCodes.FAILURE_WHEN_INSERT,wrap("ACOPERATOR",e.getMessage()));
         }
     }
 
@@ -113,7 +130,8 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
             updateById(acOperator);
             return isexist;
         }catch (Exception e){
-            throw new AcOperatorManagementException(ExceptionCodes.FAILURE_WHEN_UPDATE,wrap("ACOPERATOR",e));
+            e.printStackTrace();
+            throw new AcOperatorManagementException(ExceptionCodes.FAILURE_WHEN_UPDATE,wrap("ACOPERATOR",e.getMessage()));
         }
 
     }
@@ -175,6 +193,20 @@ public class AcOperatorServiceImpl extends ServiceImpl<AcOperatorMapper, AcOpera
         updateById(acOperator);
 
         return acOperator;
+    }
+
+    /**
+     * 查询所有操作员
+     *
+     * @return
+     * @throws AcOperatorManagementException
+     */
+    @Override
+    public List<AcOperator> queryAllOperator() throws AcOperatorManagementException {
+
+        Wrapper<AcOperator> wrapper = new EntityWrapper<AcOperator>();
+
+        return selectList(wrapper);
     }
 }
 
