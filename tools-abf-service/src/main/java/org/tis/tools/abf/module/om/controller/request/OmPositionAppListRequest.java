4a33ed492d84a2adcdc2a1b60858abc7863ce3d8
chenchao92 <Cc_Object@163.com>
2018-06-14 17:16:51 +0800

    新增OmPositionAppListRequest类

---
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/om/controller/request/OmPositionAppListRequest.java
@@ -0,0 +1,24 @@
+package org.tis.tools.abf.module.om.controller.request;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.NotBlank;
+import org.tis.tools.core.entity.request.RestRequest;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Created by chenchao
+ * Created on 2018/6/14 15:53
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class OmPositionAppListRequest extends RestRequest {
+
+    @NotBlank(message = "岗位GUID不能为空")
+    private String guidPosition;
+
+    @NotNull(message = "应用列表不能为空")
+    private List<String> appList;
+}
