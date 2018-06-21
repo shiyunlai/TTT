cb52516459f558a177a213aa624c41dd08457757
165094 <1650943282@qq.com>
2018-06-21 17:49:04 +0800

    工作组接口修改

--- tools-abf-service/src/test/java/org/tis/tools/abf/module/om/controller/OmGroupControllerTest.java
+++ tools-abf-service/src/test/java/org/tis/tools/abf/module/om/controller/OmGroupControllerTest.java
@@ -12,8 +12,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
+/**
+ * omGroup的测试类
+ *
+ * @author ljhua
+ * @date 2018/06/21
+ */
 public class OmGroupControllerTest extends BaseTest {
 
+    /**
+     * 添加根工作组
+     *
+     * @throws Exception
+     */
     @Test
     public void addRoot() throws Exception {
         RequestBuilder request = null;
@@ -21,9 +32,14 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"groupNam\":\"tip小组\",\"groupType\":\"project\",\"orgCode\":\"ORG北京总行00007\",\"groupDesc\":\"开发\"}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 添加子工作组
+     *
+     * @throws Exception
+     */
     @Test
     public void addChild() throws Exception {
         RequestBuilder request = null;
@@ -31,9 +47,14 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"groupNam\":\"RCT小组2\",\"groupType\":\"project\",\"orgCode\":\"ORG北京总行00007\"},\"groupDesc\":\"测试\",\"guidParents\":\"1006710540321443842\"}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询所有工作组列表
+     *
+     * @throws Exception
+     */
     @Test
     public void list() throws Exception {
         RequestBuilder request = null;
@@ -41,9 +62,13 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"page\":{}}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询工作组详情
+     * @throws Exception
+     */
     @Test
     public void detail() throws Exception {
         RequestBuilder request = null;
@@ -51,9 +76,13 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 删除工作组
+     * @throws Exception
+     */
     @Test
     public void delete() throws Exception {
         RequestBuilder request = null;
@@ -61,9 +90,13 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 关闭工作组
+     * @throws Exception
+     */
     @Test
     public void enable() throws Exception {
         RequestBuilder request = null;
@@ -71,9 +104,13 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 重启工作组
+     * @throws Exception
+     */
     @Test
     public void reEnable() throws Exception {
         RequestBuilder request = null;
@@ -81,9 +118,13 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 修改工作组
+     * @throws Exception
+     */
     @Test
     public void update() throws Exception {
         RequestBuilder request = null;
@@ -91,9 +132,13 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"groupCode\":\"GROUP0100000009\",\"groupNam\":\"tip小组\",\"orgCode\":\"ORG北京总行00007\",\"groupDesc\":\"开发\"}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询在此工作组的员工
+     * @throws Exception
+     */
     @Test
     public void loadEmpIn() throws Exception {
         RequestBuilder request = null;
@@ -101,19 +146,27 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"page\":{\"current\":1,\"size\":2}}")   //第一页，每页2条
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 加载不在此工作组的人员列表(同属同一机构)
+     * @throws Exception
+     */
     @Test
     public void loadEmpNotIn() throws Exception {
         RequestBuilder request = null;
-        request = post("/omGroups/GROUP0200000005/empOrgNotin/1006425335719268354")
+        request = get("/omGroups/GROUP0200000005/empOrgNotin/1006425335719268354")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content("{\"page\":{\"current\":1,\"size\":2}}")   //第一页，每页2条
+                .content("")   //第一页，每页2条
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 为工作组添加员工
+     * @throws Exception
+     */
     @Test
     public void addEmp() throws Exception {
         RequestBuilder request = null;
@@ -121,19 +174,27 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"guidGroup\":\"1007179295669379073\",\"guidEmp\":\"1007526131466887170\"}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 删除人员-工作组关联
+     * @throws Exception
+     */
     @Test
     public void deleteEmp() throws Exception {
         RequestBuilder request = null;
-        request = MockMvcRequestBuilders.delete("/1006735296508723202/empGroup")
+        request = MockMvcRequestBuilders.delete("/1006735296508723202/empGroup/1006347529454911490")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询工作组下岗位列表
+     * @throws Exception
+     */
     @Test
     public void loadPositionIn() throws Exception {
         RequestBuilder request = null;
@@ -141,9 +202,27 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"page\":{}}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询工作组下岗位列表
+     * @throws Exception
+     */
+    @Test
+    public void loadPositionInNotPage() throws Exception {
+        RequestBuilder request = null;
+        request = post("/omGroups/GROUP0200000007/positionNotPage")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content("")
+                .accept(MediaType.APPLICATION_JSON);
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
+    }
+
+    /**
+     * 查询不在此工作组的岗位
+     * @throws Exception
+     */
     @Test
     public void loadPositionNotIn() throws Exception {
         RequestBuilder request = null;
@@ -151,69 +230,98 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"page\":{}}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 为工作组新增岗位
+     * @throws Exception
+     */
     @Test
     public void addPosition() throws Exception {
         RequestBuilder request = null;
-        request = post("/omGroups/groupPosition")
+        request = post("/omGroups/position")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content("{\"groupGuid\":\"1007179295669379073\",\"posGuidlist\":[\"1006910028172492802\",\"1006907434033217538\"]}")
+                .content("{\"groupCode\":\"GROUP0200000007\",\"omPositionRequest\":{\"guidOrg\":\"1006425335719268354\",\"positionName\":\"A岗位\"," +
+                        "\"positionStatus\":\"running\",\"positionCode\":\"pos123456\",\"isleaf\":\"YES\",\"subCount\":1}}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 删除工作组岗位
+     * @throws Exception
+     */
     @Test
     public void deletePosition() throws Exception {
         RequestBuilder request = null;
-        request = MockMvcRequestBuilders.delete("/omGroups/groupPosition")
+        request = MockMvcRequestBuilders.delete("/omGroups/GROUP0200000005/position/1009404592691101698")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content("[\"1006853519543746562\"]")
+                .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 为工作组添加应用
+     * @throws Exception
+     */
     @Test
     public void addAcApp() throws Exception {
         RequestBuilder request = null;
-        request = post("/omGroups/groupApp")
+        request = post("/omGroups/app")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content("{\"groupGuid\":\"1006728472862670850\",\"appGuid\":\"997406250109054977\"}")
+                .content("{\"groupCode\":\"GROUP0200000007\",\"appGuidList\":[\"997406250109054977\"]}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询工作组下的应用
+     * @throws Exception
+     */
     @Test
     public void queryApp() throws Exception {
         RequestBuilder request = null;
-        request = post("/omGroups/GROUP0200000007/inApp")
+        request = post("/omGroups/GROUP0200000007/app")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("{\"page\":{}}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询可以为工作组添加的应用
+     * @throws Exception
+     */
     @Test
     public void queryNotInApp() throws Exception {
         RequestBuilder request = null;
-        request = post("/omGroups/GROUP0200000007/NotInApp")
+        request = get("/omGroups/GROUP0200000007/availableApp")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content("{\"page\":{}}")
+                .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 删除工作组下的应用
+     * @throws Exception
+     */
     @Test
     public void deleteApp() throws Exception {
         RequestBuilder request = null;
         request = MockMvcRequestBuilders.delete("/omGroups/groupApp")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content("{\"guid\":\"1006865716260904962\"}")
+                .content("{\"group\":\"GROUP0200000007\",\"guidApp\":\"997405585693552641\"}")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 
+    /**
+     * 查询工作组的树
+     * @throws Exception
+     */
     @Test
     public void selectTree() throws Exception {
         RequestBuilder request = null;
@@ -221,6 +329,6 @@ public class OmGroupControllerTest extends BaseTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content("")
                 .accept(MediaType.APPLICATION_JSON);
-        mvc.perform(request).andExpect(content().string(startsWith("{\"code\":\"200\",")));
+        mvc.perform(request).andExpect(content().string(startsWith("{\n\t\"code\":\"200\",")));
     }
 }
