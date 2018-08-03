4237e851241720c92a781427e8fe5e7b9f8c2354
chenchao92 <Cc_Object@163.com>
2018-08-03 09:47:18 +0800

    新增查询角色下功能行为的查询接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/dao/AcRoleFuncMapper.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/dao/AcRoleFuncMapper.java
@@ -1,7 +1,12 @@
 package org.tis.tools.abf.module.ac.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.tis.tools.abf.module.ac.entity.AcApp;
+import org.tis.tools.abf.module.ac.entity.AcFunc;
 import org.tis.tools.abf.module.ac.entity.AcRoleFunc;
+
+import java.util.List;
 
 /**
  * acRoleFunc的Mapper类
@@ -11,5 +16,13 @@ import org.tis.tools.abf.module.ac.entity.AcRoleFunc;
  */
 public interface AcRoleFuncMapper extends BaseMapper<AcRoleFunc>  {
 
+    //根据角色id查询角色已关联应用
+    List<AcApp> queryAppByRole(@Param("roleId") String roleId);
+
+    //根据角色id和应用id查询已关联功能
+    List<AcFunc> queryFuncByRole(@Param("roleId") String roleId,@Param("appId") String appId);
+
+    //根据角色,应用id查询已关联行为
+    List<AcFunc> queryBehaveByRole(@Param("roleId") String roleId,@Param("appId") String appId,@Param("funcId") String funcId);
 }
 
