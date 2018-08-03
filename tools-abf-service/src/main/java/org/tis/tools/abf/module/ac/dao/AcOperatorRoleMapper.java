91ee5038fdde6b6de49c64711b3efc143afad6ae
chenchao92 <Cc_Object@163.com>
2018-08-03 09:44:54 +0800

    新增查询已分配给角色的操作员和查询已绑定给操作员的角色接口

--- tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/dao/AcOperatorRoleMapper.java
+++ tools-abf-service/src/main/java/org/tis/tools/abf/module/ac/dao/AcOperatorRoleMapper.java
@@ -1,7 +1,12 @@
 package org.tis.tools.abf.module.ac.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.tis.tools.abf.module.ac.entity.AcOperator;
 import org.tis.tools.abf.module.ac.entity.AcOperatorRole;
+import org.tis.tools.abf.module.ac.entity.AcRole;
+
+import java.util.List;
 
 /**
  * acOperatorRole的Mapper类
@@ -11,5 +16,10 @@ import org.tis.tools.abf.module.ac.entity.AcOperatorRole;
  */
 public interface AcOperatorRoleMapper extends BaseMapper<AcOperatorRole>  {
 
+    //查询已分配给角色的操作员
+    List<AcOperator> queryOperatorByRole(@Param("roleId") String roleId);
+
+    //查询已绑定给操作员的角色
+    List<AcRole> queryRoleByOperator(@Param("operatorId") String operatorId);
 }
 
